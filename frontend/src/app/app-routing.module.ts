7f6068ef1b4920277043cb8e46cec298d4c87856
cfi <carlo@field.li>
2019-02-26 02:16:24 +0100

    create coming soon component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ComingSoonComponent } from './coming-soon/coming-soon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '**',
+    component: ComingSoonComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
